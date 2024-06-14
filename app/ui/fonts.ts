e26315183f39401c7fc902bc7fb9ee4fbe7c0f99
Wille Lee <1992season@gmail.com>
2024-06-14 19:56:26 +0900

    init

---
+++ app/ui/fonts.ts
@@ -0,0 +1,11 @@
+import { Lusitana, Inter } from 'next/font/google';
+
+export const lusitana = Lusitana({
+  weight: '700',
+  subsets: ['latin'],
+});
+
+export const inter = Inter({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
